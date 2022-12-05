5ac0ae500317de0e34f1dea4bc7ee29fa3c160f0
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2022-12-05 18:13:33 +0545

    [REPLACED]: replaced fontawesome icons with ionic icons

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const SideNav = () => {
 
-    let navItem = [
+    let navItems = [
         {
-            "id": 0,
+            "id": "home",
             "name": "Home",
-        }
+            "icon": "home-outline",
+            "activeIcon": "home",
+            "link": "#",
+            "iconSource": "ionic",
+        },
+        {
+            "id": "search",
+            "name": "Search",
+            "icon": "search-outline",
+            "activeIcon": "search",
+            "link": "#",
+            "iconSource": "ionic",
+        },
+        {
+            "id": "explore",
+            "name": "Explore",
+            "icon": "compass-outline",
+            "activeIcon": "compass",
+            "link": "#",
+            "iconSource": "ionic",
+        },
+        {
+            "id": "messages",
+            "name": "Messages",
+            "icon": "chatbubble-ellipses-outline",
+            "activeIcon": "chatbubble-ellipses",
+            "link": "#",
+            "iconSource": "ionic",
+        },
+        {
+            "id": "notifications",
+            "name": "Notifications",
+            "icon": "heart-outline",
+            "activeIcon": "heart",
+            "link": "#",
+            "iconSource": "heart",
+        },
+        {
+            "id": "create",
+            "name": "Create",
+            "icon": "duplicate-outline",
+            "activeIcon": "duplicate",
+            "link": "#",
+            "iconSource": "ionic",
+        },
     ]
     return (
         <nav className="main-nav p-3 w-[226px] fixed h-screen border-r-2 border-r-gray-300">
@@ -16,35 +58,20 @@ const SideNav = () => {
                 Binstagram
             </a>
             <ul className="nav-list px-4 pt-0 pb-4">
-                <li className="nav-item">
-                    <a href="#" className="nav-link">
-                        <span className="icon-container w-[20px] h-[20px] flex-[0_0_20px] inline-flex justify-center items-center">
-                            <FontAwesomeIcon icon={faHouse} className="w-full h-auto"/>
-                        </span>
-                       <span className="link-text pl-3">Home</span>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Search</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Explore</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Messages</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Notifications</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Create</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Profile</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">More</a>
-                </li>
+                {
+                    navItems.map((nav, index) => {
+                        return (
+                            <li className="nav-item" key={nav.id}>
+                                <a href={nav.link} className="nav-link flex items-end">
+                                    <span className="icon-container w-[20px] h-[20px] flex-[0_0_20px] inline-flex justify-center items-end">
+                                        <ion-icon name={nav.icon}></ion-icon>
+                                    </span>
+                                    <span className="link-text pl-3 leading-3">{nav.name}</span>
+                                </a>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </nav>
     )
